e628dc0048f27d0a140226b5ef0bc780b45aabc9
Rahaf Alenezi <ralenezi71@gmail.com>
2020-12-03 19:00:55 +0300

    added first component

---
+++ src/components/ItemList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import SingleItem from './SingleItem'
+
+const ItemList = () => {
+  const itemList = <SingleItem />
+  return <div>{itemList}</div>
+}
+
+export default ItemList
---
+++ src/components/SingleItem.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const SingleItem = () => {
+  return <div>hiiiiiii</div>
+}
+
+export default SingleItem
